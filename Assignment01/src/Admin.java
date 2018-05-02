d5d9cdc547d4ea2c50fd317d762d3fa5fa983bb6
Jonathan Ciminelli <Jonathan Ciminelli@10.132.141.38>
2018-05-02 12:01:51 +1000

    Started working on for assignment 2

--- Assignment01/src/Admin.java
+++ Assignment01/src/Admin.java
@@ -5,16 +5,16 @@ public class Admin extends Staff {
 	public Admin(String name, String rmitID, String password, String school, int phone, int workingHours) {
 		super(name, rmitID, password, school, phone, workingHours);
 	}
-	
+
 	public int option = 0;
 
 	@Override
 	public void menu() {
 		int availableHrs;
 		Scanner scan = new Scanner(System.in);
-		
+
 		do {
-			
+
 			System.out.println("Welcome administrator!");
 			System.out.println("1. Import data");
 			System.out.println("2. Export Data");
@@ -24,20 +24,19 @@ public class Admin extends Staff {
 			option = scan.nextInt();
 		} while (option < 0 || option >= 3);
 
-		if (option == 1)
-		{
+		if (option == 1) {
 			System.out.println("Please specify data import file: ");
-			this.option=1;
+			this.option = 1;
 		}
 
-		if (option == 2)
-			this.option=2;
+		if (option == 2) {
+			this.option = 2;
 			System.out.println("Enter export destination: ");
-
-		if (option == 3)
-			this.option=2;
+		}
+		if (option == 3) {
+			this.option = 2;
 			System.out.println("Which data would you like to modify: ");
-
+		}
 	}
 
 }
