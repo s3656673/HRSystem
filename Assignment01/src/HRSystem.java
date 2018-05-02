484abdfc5681f5f4c99f40bd9c870ea97a10f203
Jonathan Ciminelli <Jonathan Ciminelli@192.168.1.50>
2018-05-02 19:48:39 +1000

    Transferred login from hardcode to database

--- Assignment01/src/HRSystem.java
+++ Assignment01/src/HRSystem.java
@@ -2,60 +2,75 @@ import java.util.*;
 
 public class HRSystem {
 
-
-
 	public static void main(String[] args) {
 
-		String typeOfStaff;
+		String typeOfStaff = "fail";
+		String name = null;
+		String rmitID = null;
+		String password = null;
+		String school = null;
+		String phone = null;
+		int phNum = 0;
+		String course = null;
 
 		// Declare types
-
-		Staff courseCoordinator = new CourseCoordinator("Bill", "e523523", "password123", "RMIT", 441222212, 2,
-				"Departmental", "Course");
-
-		Staff casualWorker = new CasualWorker("Jill", "e123123", "password321", "RMIT", 22222, 4);
-
-		Staff approver = new Approver("Will", "e456456", "password111", "RMIT", 222, 4);
-
-		Staff admin = new Admin("Phil", "e789789", "password333", "RMIT", 222344, 6);
+		/*
+		 * Staff courseCoordinator = new CourseCoordinator("Bill", "e523523",
+		 * "password123", "RMIT", 441222212, 2, "Departmental", "Course");
+		 * 
+		 * Staff casualWorker = new CasualWorker("Jill", "e123123", "password321",
+		 * "RMIT", 22222, 4);
+		 * 
+		 * Staff approver = new Approver("Will", "e456456", "password111", "RMIT", 222,
+		 * 4);
+		 * 
+		 * Staff admin = new Admin("Phil", "e789789", "password333", "RMIT", 222344, 6);
+		 */
 
 		// Authenticate to determine type of user (approver, admin,
 		// courseCoordinator..etc)
+		/*
+		 * do { Login login = new Login(); typeOfStaff = login.getType(); if
+		 * (typeOfStaff == "fail") System.out.println("Login failed, please try again");
+		 * System.out.println(); } while (typeOfStaff == "fail");
+		 */
+
 		do {
-			Login login = new Login(courseCoordinator, casualWorker, approver,admin);
+			Login login = new Login();
 			typeOfStaff = login.getType();
 			if (typeOfStaff == "fail")
-				System.out.println("Login failed, please try again");
-			System.out.println();
+				System.out.println("Login failed, please try again" + "\n");
+			else {
+				name = login.getName();
+				rmitID = login.getRmitID();
+				password = login.getPassword();
+				school = login.getSchool();
+				phone = login.getPhone();
+				phNum = Integer.parseInt(phone);
+				course = login.getCourse();
+			}
 		} while (typeOfStaff == "fail");
-		
-	
-		
-		
-		if (typeOfStaff=="casualWorker")
-		{
-			
-			casualWorker.menu();
-			
-		}
-			
-		if (typeOfStaff=="admin")
-		{
-			
-			admin.menu();
-		}
-		
-		if (typeOfStaff=="approver")
-		{
-			
-			approver.menu();
-		}
-		
-		if (typeOfStaff=="courseCoordinator")
-		{
-			
+
+		if (typeOfStaff.equals("courseCoordinator")) {
+			Staff courseCoordinator = new CourseCoordinator(name, rmitID, password, school, phNum, 0, course, course);
 			courseCoordinator.menu();
 		}
+
+		/*
+		 * if (typeOfStaff=="casualWorker") {
+		 * 
+		 * 
+		 * }
+		 * 
+		 * if (typeOfStaff=="admin") {
+		 * 
+		 * admin.menu(); }
+		 * 
+		 * if (typeOfStaff=="approver") {
+		 * 
+		 * approver.menu(); }
+		 */
+
 	}
 
 }
--- Assignment01/src/Login.java
+++ Assignment01/src/Login.java
@@ -1,50 +1,75 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Login {
-	public String typeOfStaff = "fail";
-	
-	public Login(Staff courseCoordinator, Staff casualWorker, Staff approver, Staff admin) {
-	Scanner scan = new Scanner(System.in);
-	String username;
-	String password;
-	System.out.print("Please enter username: ");
-	username = scan.next();
-	System.out.print("Please enter password: ");
-	password = scan.next();
+	private String typeOfStaff = "fail";
+	private String name, lastname, rmitID, password, school, phone, course;
+	DatabaseHandler db = new DatabaseHandler();
 
-	// Check login
-	if (username.equals(courseCoordinator.getRmitID())) {
-		if (password.equals(courseCoordinator.getPassword()))
-			System.out.println("Login success");
-		this.typeOfStaff="courseCoordinator";
+	public Login() {
+		Scanner scan = new Scanner(System.in);
+		String username;
+		String password;
+		System.out.print("Please enter username: ");
+		username = scan.next();
+		System.out.print("Please enter password: ");
+		password = scan.next();
+
+		Connection database = db.getConnection();
+		try {
+			Statement statement = database.createStatement();
+			String sql = "select * from staff where rmitID='" + username + "' and password ='" + password + "'";
+			ResultSet rs = statement.executeQuery(sql);
+			if (rs.next()) {
+				System.out.println("Login successful");
+				this.typeOfStaff = rs.getString("Type");
+				this.name = rs.getString("FirstName");
+				this.rmitID = rs.getString("rmitID");
+				this.password = rs.getString("Password");
+				this.school = rs.getString("School");
+				this.phone = rs.getString("Phone");
+				this.course = rs.getString("Course");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	if (username.equals(casualWorker.getRmitID())) {
-		if (password.equals(casualWorker.getPassword()))
-			System.out.println("Login success");
-		this.typeOfStaff="casualWorker";
-	}
-
-	if (username.equals(approver.getRmitID())) {
-		if (password.equals(approver.getPassword()))
-			System.out.println("Login success");
-		this.typeOfStaff="approver";
-	}
-
-	if (username.equals(admin.getRmitID())) {
-		if (password.equals(admin.getPassword()))
-			System.out.println("Login success");
-		this.typeOfStaff="admin";
-	}
-
-	
-	}
-	
-	public String getType()
-	{
+	public String getType() {
 		return this.typeOfStaff;
 	}
-	
-	
-	
+
+	public String getTypeOfStaff() {
+		return typeOfStaff;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getRmitID() {
+		return rmitID;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
 }
