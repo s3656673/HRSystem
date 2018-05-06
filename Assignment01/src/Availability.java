6f1a050b30abddc23a97c4d042541db2ca7506a3
Jonathan Ciminelli <Jonathan Ciminelli@192.168.1.50>
2018-05-06 12:46:19 +1000

    Implemented check course status

--- Assignment01/src/Availability.java
+++ Assignment01/src/Availability.java
@@ -2,15 +2,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 
 public class Availability {
-
-	private String courseID;
-
-	public Availability(String courseID) {
-		this.courseID = courseID;
-	}
 
 	public void showAvailability(String courseID) {
 		String sql = null;
@@ -31,5 +24,30 @@ public class Availability {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void checkStatus(String staffID) {
+		String sql = null;
+		DatabaseHandler db = new DatabaseHandler();
+		Connection database = db.getConnection();
+
+		sql = "SELECT timetable.courseID, timetable.date, timetable.time, applied.Status" + " FROM applied"
+				+ " INNER JOIN availability on availability.ID=applied.ID"
+				+ " INNER JOIN staff on staff.rmitID=applied.rmitID"
+				+ " INNER JOIN timetable on timetable.ID=applied.timetableID" + " where applied.rmitID='" + staffID
+				+ "'";
+
+		try {
+			Statement statement = database.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			while (rs.next()) {
+				System.out.print("CourseID: " + rs.getString("courseID") + " Date: " + rs.getString("date") + " Time: "
+						+ rs.getString("time") + " Status: " + rs.getString("Status"));
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
--- Assignment01/src/CasualWorker.java
+++ Assignment01/src/CasualWorker.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 public class CasualWorker extends Staff {
 
-	
+	private String rmitID;
+
 	public CasualWorker(String rmitID, String type, String firstName, String lastName, String password, String school,
 			String phone, String workingHours) {
 		super(rmitID, type, firstName, lastName, password, school, phone, workingHours);
+		this.rmitID = rmitID;
 	}
 
 	// Declare variables
@@ -13,37 +15,37 @@ public class CasualWorker extends Staff {
 	public void menu() {
 		int option;
 		Scanner scan = new Scanner(System.in);
-		
+
 		do {
 
 			System.out.println("Welcome casual worker!");
 			System.out.println("1. View available hours");
-			System.out.println("2. State Available Hours");
-			System.out.println("3. Apply for a course");
+			System.out.println("2. Apply for a course");
+			System.out.println("3. Check course status");
 			System.out.println();
 			System.out.println("Please select an option: ");
 			option = scan.nextInt();
-		} while (option < 0 || option >= 3);
+		} while (option < 0 || option > 3);
 
-		if (option == 1)
-		{
+		if (option == 1) {
 			String courseID = null;
 			System.out.println("Please enter courseID to apply for: ");
 			courseID = scan.next();
-			Availability availability = new Availability(courseID);
-			
+			Availability availability = new Availability();
+
 			availability.showAvailability(courseID);
-			
+
 		}
 
-		if (option == 2)
-			System.out.println("See open hours");
-
-		if (option == 3)
+		if (option == 2) {
 			System.out.println("Apply for a course");
+		}
+		if (option == 3) {
+			Availability availability = new Availability();
+			availability.checkStatus(rmitID);
+
+		}
 
 	}
-	
-
 
 }
