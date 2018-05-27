05b2db99818b8af99f812be8b5d35bf24dcc48e8
Jonathan Ciminelli <Jonathan Ciminelli@192.168.1.50>
2018-05-27 12:48:10 +1000

    Implemented JUnit testing.

--- Assignment01/src/JUnit_Test.java
+++ Assignment01/src/JUnit_Test.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.*;
 import org.junit.jupiter.api.Test;
 
-public class test {
+public class JUnit_Test {
 
 	Timetable timetable;
 	Rates rates;
