a33abf70ba3d3d5d5773562ffd7944fd8ed14559
Jonathan Ciminelli <Jonathan Ciminelli@192.168.1.50>
2018-05-27 12:18:52 +1000

    Code implementation

--- Assignment01/src/test.java
+++ Assignment01/src/test.java
@@ -5,35 +5,41 @@ import org.junit.jupiter.api.Test;
 
 class test {
 
+	@Test
+	// Test viewing timetable
+	// This test is expected to pass as there is a record for ISYS1118 in the
+	// database.
+	void testTimetable() {
+		Timetable timetable = new Timetable();
+		String result = timetable.viewTimetable("ISYS1118");
+		assertEquals("ID: 1 FirstName: Bill LastName: Billson Date: 1/08/2018 Time:04:00", result);
+	}
+
+	@Test
+	// Test input rate
 	/*
-	 * @Test //Test viewing timetable //This test is expected to pass as there is a
-	 * record for ISYS1118 in the database. void testTimetable() { Timetable
-	 * timetable = new Timetable(); String result =
-	 * timetable.viewTimetable("ISYS1118");
-	 * assertEquals("ID: 1 FirstName: Bill LastName: Billson Date: 1/08/2018 Time:04:00"
-	 * , result); }
-	 * 
-	 * //Test input rates
-	 * 
 	 * This test is expected to fail if the record already exists in the database,
 	 * as the inputRates method imports into the database. However, if no record
 	 * exists, it will be expected to pass.
-	 * 
-	 * @Test void inputRates() { Rates rates = new Rates(); String result =
-	 * rates.inputRates(); assertEquals("Data imported successfully",result); }
 	 */
+	void inputRates() {
+		Rates rates = new Rates();
+		String result = rates.inputRates();
+		assertEquals("Data imported successfully", result);
+	}
 
+	@Test
 	/*
-	 * @Test
-	 * 
 	 * This test checks if the availability class can manipulate the data in the SQL
 	 * database, if the test has passed, the boolean will return true, therefore
 	 * test will pass. Otherwise, if an SQLException occurs, the boolean will return
 	 * false, therefore test will fail.
-	 * 
-	 * void apply() { Availability availability = new Availability(); boolean result
-	 * = availability.apply("ISYS1118", "e123123"); assertTrue(result); }
 	 */
+	void apply() {
+		Availability availability = new Availability();
+		boolean result = availability.apply("ISYS1118", "e123123");
+		assertTrue(result);
+	}
 
 	@Test
 
